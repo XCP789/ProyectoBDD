4bea92596ed209bba9c6392d4e9b0d70567165a2
doris Cruz <dbcruzhdez@gmail.com>
2025-05-13 10:23:59 -0700

    Cambios en principal

--- Principal.xaml.cs
+++ Principal.xaml.cs
@@ -4,11 +4,20 @@ public partial class Principal : ContentPage
 {
     public Command<string> NavigateCommand { get; }
     public Principal()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         NavigateCommand = new Command<string>(NavigateToPage);
         BindingContext = this;
     }
+    private readonly List<string> images = new()
+    {
+        "vigia.jpg",
+        "brisa.jpg",
+        "mujeres.jpg",
+        "rojos.jpg",
+        "amarillos.jpg"
+    };
+    private int currentIndex = 0;
     private async void NavigateToPage(string SistemaTransporte)
     {
         //if (!string.IsNullOrEmpty(pageName))
@@ -20,5 +29,29 @@ public partial class Principal : ContentPage
     private async void IrSistemTrans(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SistemaTransporte());
+    }
+
+
+    private void OnPreviousClicked(object sender, EventArgs e)
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            UpdateImage();
+        }
+    }
+
+    private void OnNextClicked(object sender, EventArgs e)
+    {
+        if (currentIndex < images.Count - 1)
+        {
+            currentIndex++;
+            UpdateImage();
+        }
+    }
+
+    private void UpdateImage()
+    {
+        imageDisplay.Source = images[currentIndex];
     }
 }
